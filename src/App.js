30e00053021ca4b7bbd3e762cd1ff50af2313ba3
tenoclock <enr@bk.ru>
2024-02-28 01:29:34 +0500

    finish

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import { Route } from "react-router-dom";
 import "./assets/css/style.css";
 
 import Home from "./pages/Home";
-// import Portfolio from "./pages/Portfolio";
-// import About from "./pages/About";
 
 function App() {
-  // return <Route exact path="/" component={About} />;
-  // return <Route exact path="/" component={Portfolio} />;
   return <Route exact path="/" component={Home} />;
 }
 
